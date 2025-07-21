5ec2faa7128ea0cb7fbf79f8675c6247ab4415d1
krasotun <servrostov@gmail.com>
2025-07-21 21:58:29 +0300

    add array required validator

---
+++ src/validators/array-required.test.js
@@ -0,0 +1,30 @@
+import { beforeEach, describe, expect, it } from "@jest/globals";
+import { numberRequiredValidator } from "./number-required";
+import { arrayRequiredValidator } from "./array-required";
+
+describe("#arrayRequired", () => {
+  let arr;
+
+  beforeEach(() => {
+    arr = [1, 2, 3];
+  });
+
+  it("should return false if value not a array", () => {
+    arr = "num";
+    expect(arrayRequiredValidator(arr)).toBe(false);
+  });
+
+  it("should return false on null", () => {
+    arr = null;
+    expect(numberRequiredValidator(arr)).toBe(false);
+  });
+
+  it("should return false on undefined", () => {
+    arr = undefined;
+    expect(arrayRequiredValidator(arr)).toBe(false);
+  });
+
+  it("should return true if array", () => {
+    expect(arrayRequiredValidator(arr)).toBe(true);
+  });
+});
