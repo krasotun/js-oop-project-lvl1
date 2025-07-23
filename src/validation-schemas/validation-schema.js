238b91665a23c9706c0b5b61924ab10e00a6850c
krasotun <servrostov@gmail.com>
2025-07-23 23:00:45 +0300

    add valid

--- src/validation-schemas/validation-schema.js
+++ src/validation-schemas/validation-schema.js
@@ -6,7 +6,7 @@ class ValidationSchema {
   isValid(value) {
     return Object.values(this.validators)
       .every(
-        ({ validator, params } ) => validator(value, params)
+        ({ validator, params }) => validator(value, params)
       );
   }
 
